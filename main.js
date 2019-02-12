0198fca5733d40cf115ce6002f02a5597af63b8a
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-02-12 11:09:31 +0100

    more methods implemented
    - some fixes

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core');
-const request = require('request-promise-native');
 const crypto = require(__dirname + '/lib/crypto');
 const Bring = require(__dirname + '/lib/bring');
 let adapter;
@@ -47,6 +46,22 @@ function startAdapter(options) {
         });
     });
 
+    adapter.on('stateChange', async (id, state) => {
+        if (!id || !state || state.ack) return;
+        adapter.log.warn(`[STATE] Changed ${id} to ${state.val}`);
+        const listId = id.split('.')[2];
+        const method = id.split('.').pop();
+
+        if (method === 'search') {
+            const res = await bring.searchItem(state.val, listId);
+            adapter.log.warn(`[SEARCH] Received ${JSON.stringify(res)}`);
+            adapter.setState(id, state.val, true);
+        } else if (method === 'removeItem') {
+            const res = await bring.removeItem(listId, state.val);
+            adapter.log.warn(`[REMOVE] Received ${JSON.stringify(res)}`);
+        }
+    });
+
     return adapter;
 } // endStartAdapter
 
@@ -60,8 +75,9 @@ async function main() {
         password: password
     });
 
-
     await tryLogin();
+
+    adapter.setState('info.user', bring.name, true);
 
     const bringLists = await bring.loadLists();
 
@@ -72,21 +88,92 @@ async function main() {
 
         promises.push(adapter.setObjectNotExistsAsync(entry.listUuid, {
             type: 'channel',
-            'common': {
-                'name': entry.name,
+            common: {
+                name: entry.name
             },
-            'native': {}
+            native: {}
         }));
 
         promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.content`, {
             type: 'state',
             common: {
-                role: 'list',
+                role: 'list.json',
                 name: 'Content',
                 desc: `Content of ${entry.name}`,
                 read: true,
                 write: false,
-                def: []
+                type: 'string',
+                def: '[]'
+            },
+            native: {}
+        }));
+
+        promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.recentContent`, {
+            type: 'state',
+            common: {
+                role: 'list.json',
+                name: 'Recent Content',
+                desc: `Recent Content of ${entry.name}`,
+                read: true,
+                write: false,
+                type: 'string',
+                def: '[]'
+            },
+            native: {}
+        }));
+
+        promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.users`, {
+            type: 'state',
+            common: {
+                role: 'list.json',
+                name: 'Recent Content',
+                desc: `Recent Content of ${entry.name}`,
+                read: true,
+                write: false,
+                type: 'string',
+                def: '[]'
+            },
+            native: {}
+        }));
+
+        promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.search`, {
+            type: 'state',
+            common: {
+                role: 'text',
+                name: 'Search',
+                desc: 'Search for an item',
+                read: true,
+                write: true,
+                type: 'string',
+                def: ''
+            },
+            native: {}
+        }));
+
+        promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.searchResult`, {
+            type: 'state',
+            common: {
+                role: 'text',
+                name: 'Search Result',
+                desc: 'Result of Search',
+                read: true,
+                write: false,
+                type: 'string',
+                def: ''
+            },
+            native: {}
+        }));
+
+        promises.push(adapter.setObjectNotExistsAsync(`${entry.listUuid}.removeItem`, {
+            type: 'state',
+            common: {
+                role: 'text',
+                name: 'Remove Item',
+                desc: 'Remove item from List',
+                read: true,
+                write: true,
+                type: 'string',
+                def: ''
             },
             native: {}
         }));
@@ -94,8 +181,17 @@ async function main() {
         await Promise.all(promises);
 
         bring.getItems(entry.listUuid).then(data => {
-            adapter.log.warn(`[DATA] Items loaded: ${JSON.stringify(data)}`);
-        }).catch((e) => {
+            adapter.log.warn(`[DATA] Items from ${entry.listUuid} loaded: ${JSON.stringify(data)}`);
+            adapter.setState(`${entry.listUuid}.content`, JSON.stringify(data.purchase), true);
+            adapter.setState(`${entry.listUuid}.recentContent`, JSON.stringify(data.recently), true);
+        }).catch(e => {
+            adapter.log.warn(e);
+        });
+
+        bring.getAllUsersFromList(entry.listUuid).then(data => {
+            adapter.log.warn(`[DATA] Users from ${entry.listUuid} loaded: ${JSON.stringify(data)}`);
+            adapter.setState(`${entry.listUuid}.users`, JSON.stringify(data.users), true);
+        }).catch(e => {
             adapter.log.warn(e);
         });
     } // endFor
