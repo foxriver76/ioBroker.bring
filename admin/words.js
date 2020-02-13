7bdd40054b672fecdd3ac5418e601d00d50d29aa
Moritz Heusinger <moritz.heusinger@gmail.com>
2020-02-13 21:01:06 +0100

    fallback to old encryption if window.crypto not defined
    - and tell user more about encryption
    - 1.7.1

--- admin/words.js
+++ admin/words.js
@@ -146,4 +146,40 @@ systemDictionary = {
         "pl": `Odbiornik telegramu`,
         "zh-cn": `电报接收器`
     },
+    "Decryption but no crypto lib": {
+        "en": `Credentials encrypted with AES-256-CEC but browser does not provide a decryption possibility!`,
+        "de": `Mit AES-256-CEC verschlüsselte Anmeldeinformationen, aber der Browser bietet keine Entschlüsselungsmöglichkeit!`,
+        "ru": `Учетные данные зашифрованы с помощью AES-256-CEC, но браузер не обеспечивает возможность расшифровки!`,
+        "pt": `Credenciais criptografadas com o AES-256-CEC, mas o navegador não oferece a possibilidade de descriptografia!`,
+        "nl": `Referenties gecodeerd met AES-256-CEC maar browser biedt geen decoderingsmogelijkheid!`,
+        "fr": `Les informations d'identification cryptées avec AES-256-CEC mais le navigateur ne fournit pas de possibilité de décryptage!`,
+        "it": `Credenziali crittografate con AES-256-CEC ma il browser non offre la possibilità di decrittazione!`,
+        "es": `¡Credenciales cifradas con AES-256-CEC pero el navegador no ofrece la posibilidad de descifrado!`,
+        "pl": `Poświadczenia zaszyfrowane za pomocą AES-256-CEC, ale przeglądarka nie zapewnia możliwości odszyfrowania!`,
+        "zh-cn": `使用AES-256-CEC加密的凭证，但浏览器不提供解密的可能性！`
+    },
+    "Provided key too short on encrypt": {
+        "en": `The provided key is too short, fell back to standard encryption!`,
+        "de": `Der mitgelieferte Schlüssel ist zu kurz, auf Standardverschlüsselung zurückgegriffen!`,
+        "ru": `Предоставленный ключ слишком короткий, вернулся к стандартному шифрованию!`,
+        "pt": `A chave fornecida é muito curta, voltou à criptografia padrão!`,
+        "nl": `De verstrekte sleutel is te kort, viel terug op standaardcodering!`,
+        "fr": `La clé fournie est trop courte, retombée au chiffrement standard!`,
+        "it": `La chiave fornita è troppo corta, è tornato alla crittografia standard!`,
+        "es": `La clave proporcionada es demasiado corta, ¡recurrió al cifrado estándar!`,
+        "pl": `Podany klucz jest za krótki, wrócił do standardowego szyfrowania!`,
+        "zh-cn": `提供的密钥太短，无法使用标准加密！`
+    },
+    "Crypto not available on encrypt": {
+        "en": `Your browser does not provide a encryption possibility for AES-256-CEC. Fallback to standard encryption!`,
+        "de": `Ihr Browser bietet keine Verschlüsselungsmöglichkeit für AES-256-CEC. Rückgriff auf Standardverschlüsselung!`,
+        "ru": `Ваш браузер не предоставляет возможности шифрования для AES-256-CEC. Откат к стандартному шифрованию!`,
+        "pt": `O seu navegador não oferece uma possibilidade de criptografia para o AES-256-CEC. Fallback para criptografia padrão!`,
+        "nl": `Uw browser biedt geen coderingsmogelijkheid voor AES-256-CEC. Terugvallen op standaardcodering!`,
+        "fr": `Votre navigateur ne fournit pas de possibilité de cryptage pour AES-256-CEC. Retour au cryptage standard!`,
+        "it": `Il tuo browser non offre la possibilità di crittografia per AES-256-CEC. Fallback alla crittografia standard!`,
+        "es": `Su navegador no proporciona una posibilidad de cifrado para AES-256-CEC. ¡Recurrir al cifrado estándar!`,
+        "pl": `Twoja przeglądarka nie zapewnia możliwości szyfrowania dla AES-256-CEC. Powrót do standardowego szyfrowania!`,
+        "zh-cn": `您的浏览器未提供AES-256-CEC加密的可能性。退回标准加密！`
+    }
 };
